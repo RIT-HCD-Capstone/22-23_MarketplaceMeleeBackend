94679a00874401dcde3f7da96d865acc6ebb9de8
jcleme <clemensjosh@protonmail.com>
2023-02-10 21:29:32 -0500

    update port def

--- main.ts
+++ main.ts
@@ -115,7 +115,7 @@ server.register(async function (server) {
 
 const start = async () => {
   try {
-    await server.listen({ port: process.env.PORT || 3883, host: '0.0.0.0' })
+    await server.listen({ port: 3883, host: '0.0.0.0' })
 
     const address = server.server.address()
     const port = typeof address === 'string' ? address : address?.port
