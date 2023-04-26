b40a6b38a956608121a3e0c19db8f7a96d04df25
jcleme <clemensjosh@protonmail.com>
2023-04-26 16:41:28 -0400

    update effectType to be more useful

--- lib/Item.ts
+++ lib/Item.ts
@@ -10,201 +10,177 @@ export default interface Item {
 }
 
 /** The rarity of the items, how commonly they appear in the shop */
-export type Rarity = 'Common' | 'Uncommon' | 'Rare' | 'Legendary'
+export type Rarity = "Common" | "Uncommon" | "Rare" | "Legendary";
 
 /** The stat that the item effects */
-export type AffectedStats = 'Attack' | 'Defend' | 'Range' | 'Value'
+export type AffectedStats = "Attack" | "Defend" | "Range" | "Value";
 /** the type of effect */
-export type EffectType = 'additive' | 'multiplicative'
+export type EffectType = "+" | "x";
 
 /** The effected stat, type, and volume of the change */
 export interface Effect {
-  stat: AffectedStats
-  type: EffectType
-  amount: number
+  stat: AffectedStats;
+  type: EffectType;
+  amount: number;
 }
 
-export let AllItems: Item[] =
-  [{
-    name: "Spear",
-    description: "Double attack range",
-    rarity: "Legendary",
-    price: 30,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Range",
-      type: "multiplicative",
-      amount: 2
-    }
+export let AllItems: Item[] = [{
+  name: "Spear",
+  description: "Double attack range",
+  rarity: "Legendary",
+  price: 30,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Range",
+    type: "x",
+    amount: 2,
   },
-
-  {
-    name: "Pointy Stick",
-    description: "Increase attack damage by 30",
-    rarity: "Rare",
-    price: 40,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Attack",
-      type: "additive",
-      amount: 30
-    }
+}, {
+  name: "Pointy Stick",
+  description: "Increase attack damage by 30",
+  rarity: "Rare",
+  price: 40,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Attack",
+    type: "+",
+    amount: 30,
   },
-
-  {
-    name: "Non copyright infringing blue hedgehog shoes",
-    description: "Increase your movement range by half your starting range",
-    rarity: "Rare",
-    price: 40,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Range",
-      type: "multiplicative",
-      amount: 1.5
-    }
+}, {
+  name: "Non copyright infringing blue hedgehog shoes",
+  description: "Increase your movement range by half your starting range",
+  rarity: "Rare",
+  price: 40,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Range",
+    type: "x",
+    amount: 1.5,
   },
-
-  {
-    name: "Bucket of Teeth",
-    description: "You now have more teeth. Do 2 extra damage per attack",
-    rarity: "Uncommon",
-    price: 5,
-    consumable: false,
-    numUses: 5,
-    effect: {
-      stat: "Attack",
-      type: "additive",
-      amount: 2
-    }
+}, {
+  name: "Bucket of Teeth",
+  description: "You now have more teeth. Do 2 extra damage per attack",
+  rarity: "Uncommon",
+  price: 5,
+  consumable: false,
+  numUses: 5,
+  effect: {
+    stat: "Attack",
+    type: "+",
+    amount: 2,
   },
-
-  {
-    name: "'Axe' Body Spray",
-    description: "Nobody can move within attack range of you next turn",
-    rarity: "Rare",
-    price: 15,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Range",
-      type: "additive",
-      amount: 0
-    }
+}, {
+  name: "'Axe' Body Spray",
+  description: "Nobody can move within attack range of you next turn",
+  rarity: "Rare",
+  price: 15,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Range",
+    type: "+",
+    amount: 0,
   },
-
-  {
-    name: "Bucket of Rocks",
-    description: "You now have a bucket of rocks. Do 5 extra damage per attack, can attack from your move range",
-    rarity: "Rare",
-    price: 15,
-    consumable: false,
-    numUses: 5,
-    effect: {
-      stat: "Attack",
-      type: "additive",
-      amount: 5
-    }
+}, {
+  name: "Bucket of Rocks",
+  description:
+    "You now have a bucket of rocks. Do 5 extra damage per attack, can attack from your move range",
+  rarity: "Rare",
+  price: 15,
+  consumable: false,
+  numUses: 5,
+  effect: {
+    stat: "Attack",
+    type: "+",
+    amount: 5,
   },
-
-  {
-    name: "Bond",
-    description: "Gain 80 in two turns",
-    rarity: "Uncommon",
-    price: 20,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Value",
-      type: "additive",
-      amount: 80
-    }
+}, {
+  name: "Bond",
+  description: "Gain 80 in two turns",
+  rarity: "Uncommon",
+  price: 20,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Value",
+    type: "+",
+    amount: 80,
   },
-
-  {
-    name: "Sacrificial Dagger",
-    description: "Your next attack grants quadruple the value of the damage dealt",
-    rarity: "Legendary",
-    price: 50,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Value",
-      type: "multiplicative",
-      amount: 4
-    }
+}, {
+  name: "Sacrificial Dagger",
+  description:
+    "Your next attack grants quadruple the value of the damage dealt",
+  rarity: "Legendary",
+  price: 50,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Value",
+    type: "x",
+    amount: 4,
   },
-
-  {
-    name: "Gilded Shield",
-    description: "Block 3 times, gain 80",
-    rarity: "Uncommon",
-    price: 20,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Value",
-      type: "additive",
-      amount: 80
-    }
+}, {
+  name: "Gilded Shield",
+  description: "Block 3 times, gain 80",
+  rarity: "Uncommon",
+  price: 20,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Value",
+    type: "+",
+    amount: 80,
   },
-
-  {
-    name: "Bucket of Dragon Teeth",
-    description: "You now have Spyro's teeth. Do 45 extra damage per attack",
-    rarity: "Legendary",
-    price: 50,
-    consumable: false,
-    numUses: 3,
-    effect: {
-      stat: "Attack",
-      type: "additive",
-      amount: 45
-    }
+}, {
+  name: "Bucket of Dragon Teeth",
+  description: "You now have Spyro's teeth. Do 45 extra damage per attack",
+  rarity: "Legendary",
+  price: 50,
+  consumable: false,
+  numUses: 3,
+  effect: {
+    stat: "Attack",
+    type: "+",
+    amount: 45,
   },
-
-  {
-    name: "Survey Contract",
-    description: "Sell at 3 different Marketplaces, gain double the value you gained from selling at them",
-    rarity: "Uncommon",
-    price: 15,
-    consumable: false,
-    numUses: 1,
-    effect: {
-      stat: "Value",
-      type: "multiplicative",
-      amount: 2
-    }
+}, {
+  name: "Survey Contract",
+  description:
+    "Sell at 3 different Marketplaces, gain double the value you gained from selling at them",
+  rarity: "Uncommon",
+  price: 15,
+  consumable: false,
+  numUses: 1,
+  effect: {
+    stat: "Value",
+    type: "x",
+    amount: 2,
   },
-
-  {
-    name: "Tree Branch",
-    description: "Do 10 extra damage on your next attack",
-    rarity: "Common",
-    price: 10,
-    consumable: true,
-    numUses: 1,
-    effect: {
-      stat: "Attack",
-      type: "additive",
-      amount: 10
-    }
+}, {
+  name: "Tree Branch",
+  description: "Do 10 extra damage on your next attack",
+  rarity: "Common",
+  price: 10,
+  consumable: true,
+  numUses: 1,
+  effect: {
+    stat: "Attack",
+    type: "+",
+    amount: 10,
   },
-
-  {
-    name: "Armor",
-    description: "Save 10 more value on your next save",
-    rarity: "Common",
-    price: 5,
-    consumable: true,
-    numUses: 1,
-    effect: {
-      stat: "Defend",
-      type: "additive",
-      amount: 10
-    }
-  }]
-
-
+}, {
+  name: "Armor",
+  description: "Save 10 more value on your next save",
+  rarity: "Common",
+  price: 5,
+  consumable: true,
+  numUses: 1,
+  effect: {
+    stat: "Defend",
+    type: "+",
+    amount: 10,
+  },
+}];
